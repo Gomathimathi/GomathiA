955025bac53c6f53bfba5121ecfb79138f21c87e
Gomathimathi <gomathi.acse@ksrce.ac.in>
2016-07-22 17:42:16 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,13 @@
+public class PositiveOrNegative{
+public static void main(String[] args) {
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+if(a<0){
+System.out.println("negative");
+}
+else if(a>0){
+System.out.println("positive");
+}
+else{
+System.out.println("zero");
+} } }
