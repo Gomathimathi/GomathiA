712574edccd3bec2194e05fe03b34a2ad1164eca
Gomathi <gomathi.acse@ksrce.ac.in>
2016-08-31 09:42:10 +0530

    Add files via upload

---
+++ ReverseString.java
@@ -0,0 +1,11 @@
+package guvi;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+			String str="guvi";
+			StringBuffer buffer=new StringBuffer(str);
+			System.out.println(buffer.reverse());
+	}
+
+}
